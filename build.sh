725a5d1b68cf2c1d23bc39ff73c54ba308d11398
Biswajit Sahoo <b4608323@gmail.com>
2026-04-14 19:46:45 +0530

    Refactor: Moved static files to social app for better Render compatibility

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Diagnostic: List files to see if they exist
-ls -R static
+ls -R social/static
 
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
--- community/settings.py
+++ community/settings.py
@@ -126,13 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# DIagnostics for Render build
-print(f"DEBUG: BASE_DIR is {BASE_DIR}")
-print(f"DEBUG: static folder exists: {(BASE_DIR / 'static').exists()}")
-print(f"DEBUG: STATICFILES_DIRS is {STATICFILES_DIRS}")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise settings for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
