68e97973e69ea21722b1b3066863b4d35ab43af1
Biswajit Sahoo <b4608323@gmail.com>
2026-04-14 19:09:41 +0530

    Fix: Added missing static and media files for Render deployment

--- community/settings.py
+++ community/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise settings for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
