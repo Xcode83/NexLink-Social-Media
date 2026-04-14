47b34f3a6db6347697a4c75a015b8095a8e63e02
Biswajit Sahoo <b4608323@gmail.com>
2026-04-14 20:00:53 +0530

    Brute Force: Manual filesystem copy of static files to resolve collectstatic failure

--- build.sh
+++ build.sh
@@ -7,5 +7,12 @@ pip install -r requirements.txt
 # Diagnostic: List files to see if they exist
 ls -R static
 
+# BRUTE FORCE: Manually copy files because Django's collectstatic is failing
+mkdir -p staticfiles
+cp -r static/* staticfiles/
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Final Verification
+ls -R staticfiles
